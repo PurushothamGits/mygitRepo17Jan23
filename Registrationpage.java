726dcf64eb2d2ba95e488422e667df67f80909e0
PurushothamGits <Purushothamgit@gmail.com.com>
2023-02-06 23:51:16 +0530

    my task completed

--- Registrationpage.java
+++ Registrationpage.java
@@ -1,4 +1,11 @@
 This is java reg page
+<<<<<<< Updated upstream
 
  my manager work completed
 
+
+1st day java lines of code completed
+2nd day java lines of code completed
+3rd day java lines of code completed
+4th day java lines of code completed
+5th day java lines of code completed
